60077e7d74ce3adf5b052c457bb01d16169af133
kKsk <alonzomax1210@gmail.com>
2024-01-09 22:30:24 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -30,11 +30,7 @@ export default defineUserConfig({
 
     displayFooter: true,
     footer: "使用 <a href=\"https://theme-hope.vuejs.press/zh/\" target=\"_blank\">VuePress Theme Hope</a> 主题 | MIT 协议",
-    copyright: "版权所有 &copy 2023 ~ Present <a href=\"https://github.com/kKsk03\" target=\"_blank\">kKsk</a>",
-
-    plugins: {
-      search: true
-    }
+    copyright: "版权所有 &copy 2023 ~ Present <a href=\"https://github.com/kKsk03\" target=\"_blank\">kKsk</a>"
   }),
 
   plugins: [
@@ -53,6 +49,50 @@ export default defineUserConfig({
       appId: "5OGLWFJIJU",
       apiKey: "2fd19ed216eba033fb406e85229d6bc3",
       indexName: "xenon-rs",
+
+      // 翻译
+      translations: {
+        button: {
+          buttonText: "搜索Xenon Docs",
+          buttonAriaLabel: '搜索Xenon Docs',
+        },
+        modal: {
+          searchBox: {
+            resetButtonTitle: '清除记录',
+            resetButtonAriaLabel: '清除记录',
+            cancelButtonText: '取消',
+            cancelButtonAriaLabel: '取消',
+          },
+          startScreen: {
+            recentSearchesTitle: '最近的搜索结果',
+            noRecentSearchesText: '无最近的搜索结果',
+            saveRecentSearchButtonTitle: '保存该结果',
+            removeRecentSearchButtonTitle: '从记录中删除',
+            favoriteSearchesTitle: '收藏',
+            removeFavoriteSearchButtonTitle: '从收藏中删除',
+          },
+          errorScreen: {
+            titleText: '无法获取结果',
+            helpText: 'BYD你是不是没网',
+          },
+          footer: {
+            selectText: '选定',
+            selectKeyAriaLabel: 'Enter',
+            navigateText: '上下选择',
+            navigateUpKeyAriaLabel: 'Arrow up',
+            navigateDownKeyAriaLabel: 'Arrow down',
+            closeText: '关闭',
+            closeKeyAriaLabel: 'Escape key',
+            searchByText: '搜索服务提供商：',
+          },
+          noResultsScreen: {
+            noResultsText: '没有如下的搜索结果：',
+            suggestedQueryText: '尝试搜索',
+            reportMissingResultsText: '你相信这个查询应该返回结果？',
+            reportMissingResultsLinkText: '让我们揭晓',
+          }
+        }
+      }
     })
   ]
 });
